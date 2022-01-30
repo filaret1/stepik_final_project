b8dde5b8be57fe83ae5aebd3a7c66ff07b65fdf0
Андрей <andreigovor77@gmail.com>
2022-01-30 22:22:06 +0300

    добавлены переходы между страницами

--- conftest.py
+++ conftest.py
@@ -1,20 +1,24 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-
-
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default="en",
-                     help="language")
-
+    parser.addoption('--browser_name', action='store', default='chrome',)
+    parser.addoption('--language', action="store", default='en', help="Choose from langs: (en/ru/es/...)")
 
 @pytest.fixture(scope="function")
 def browser(request):
+    browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    if (browser_name == "chrome"):
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+
+    else:
+        print("Browser<browser_name> still is not implemented")
     yield browser
-    print("\nquit browser..")
+    print("\nQuit browser...")
     browser.quit()
