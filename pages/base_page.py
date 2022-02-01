c8f246d1e3d7c353793ac8322afea38a17e4f277
Андрей <andreigovor77@gmail.com>
2022-02-01 23:00:15 +0300

    Задание: отрицательные проверки

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 
 class BasePage():
@@ -31,3 +35,27 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+
--- test_product_page.py
+++ test_product_page.py
@@ -15,3 +15,27 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.guest_can_see_product_name()
     time.sleep(2)
     page.product_price_is_correct()
+
+@pytest.mark.xfail(reason="test should fall because guest can see success message after adding product to basket")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.guest_can_add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    time.sleep(4)
+    page.check_message_about_adding_to_basket()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.check_success_message_on_product_page()
+
+@pytest.mark.xfail(reason="test should fall because success message is not disappeared")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.guest_can_add_product_to_basket()
+    page.check_success_message_is_disappeared()
