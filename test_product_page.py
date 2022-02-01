eb0e96124a040260f66f224aa78410b96a2574ac
Андрей <andreigovor77@gmail.com>
2022-02-01 18:13:07 +0300

    добавление в корзину со страницы товара

---
+++ test_product_page.py
@@ -0,0 +1,17 @@
+from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
+import time
+import pytest
+
+@pytest.mark.parametrize('promo_offer', ["?promo=offer0", "?promo=offer1", "?promo=offer2", "?promo=offer3", "?promo=offer4", "?promo=offer5", "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail(reason="fixing this bug right now")), "?promo=offer8", "?promo=offer9"])
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    page = ProductPage(browser, link)
+    page.open()
+    page.guest_can_add_product_to_basket()
+    page.solve_quiz_and_get_code()
+    time.sleep(2)
+    page.guest_can_see_product_name()
+    time.sleep(2)
+    page.product_price_is_correct()
